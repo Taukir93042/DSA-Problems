b1ff9ff971827543b0110be4f6f69c826e827a2b
Taukir Alam <mdkaifalam93042@gmail.com>
2026-08-03 01:07:41 +0530

    basic math

---
+++ basic-fundamental/basic-math.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+// count all digints of a number
+// int countDigits(int n){
+//   int count=0;
+//   while(n){
+//     n /= 10;
+//     count++;
+//   }
+//   return count;
+// }
+
+
+
+int main(){
+   int n=77890099;
+   cout<<countDigits(n);
+  return 0;
+}
