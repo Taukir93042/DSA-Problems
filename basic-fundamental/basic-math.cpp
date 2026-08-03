a47646ee4b8d2514135bad34685452147666fe6e
Taukir Alam <mdkaifalam93042@gmail.com>
2026-08-03 21:06:16 +0530

    recursion basic

--- basic-fundamental/basic-math.cpp
+++ basic-fundamental/basic-math.cpp
@@ -13,8 +13,34 @@ using namespace std;
 
 
 
+// reverse digits
+// int reverseDegits(int n){
+//   int ans=0;
+//   while(n){
+//     int rem = n%10;
+//     n /= 10;
+//     ans = ans*10 + rem;
+    
+//   }
+//   return ans;
+// }
+
+
+// //reverse array using recursion
+
+// void reverseArray(int arr[], int low, int high){
+//       if(low>high) return;
+//          swap(arr[low],arr[high]);
+        
+//       reverseArray(arr,low+1,high-1);
+// }
 int main(){
-   int n=77890099;
-   cout<<countDigits(n);
+    int arr[] = {10, 20, 30, 40, 50};
+    int size = sizeof(arr)/sizeof(arr[0]);
+   reverseArray(arr,0,size-1);
+
+   for(int i=0; i<size; i++){
+    cout<<arr[i]<<" ";
+   }
   return 0;
 }
